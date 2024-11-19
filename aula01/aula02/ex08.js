4cbed57650569cc6abe3e724d576db3341701bf9
Dinis James <Dinisjames145@gmail.com>
2024-11-19 19:14:05 +0200

    comit

---
+++ aula01/aula02/ex08.js
@@ -0,0 +1,5 @@
+var vel = 79
+console.log(`A velocidade do seu carro e de ${vel} kmh`)
+if (vel > 60)
+    console.log("Voce ultrapassou a linha da velocidade! voce foi multado.")
+console.log('Dirija sempre com seguranca!')
---
+++ aula01/aula02/ex09.js
@@ -0,0 +1,7 @@
+var pais = 'lunar'
+console.log(`vivendo em ${pais}`)
+if (pais == 'America') {
+    console.log('voce e americano')
+}else {
+    console.log('Voce e estrangeiro')
+}
